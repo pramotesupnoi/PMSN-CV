8f53c25d2f765572d524253c4ebec37f3df89932
Pramote Supnoi <31182465+pramotesupnoi@users.noreply.github.com>
2022-01-12 16:49:08 +0700

    add GA

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import TopProfile from './components/TopProfile';
 import AboutMe from './components/AboutMe';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactGA from 'react-ga';
+
 function App() {
+  const TRACKING_ID = "UA-216979659-2";
+  ReactGA.initialize(TRACKING_ID);
+  
   return (
     <div className="container-lg py-3">
       <div className="row">
