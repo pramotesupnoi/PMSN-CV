0904df0661613a4338a78a089290c07e7c0ee552
Pramote Supnoi <pramotesupnoi@gmail.com>
2022-01-17 11:17:19 +0700

    add download pdf

--- src/components/TopProfile.js
+++ src/components/TopProfile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TopProfileContactItem from "./TopProfileContactItem";
+import cv_file from './../pramote-supnoi-cv.pdf'
 
 export default function TopProfile() {
   return (
@@ -16,16 +17,20 @@ export default function TopProfile() {
             <div className="col-12 col-md-9 col-lg-12">
               <div className="text-center mt-4">
                 <h3>Pramote Supnoi</h3>
-                <h5 className="fw-light text-black-50">Full-stack developer</h5>
+                <h5 className="fw-light text-black-50">Full stack developer</h5>
               </div>
               <div className="row mt-4">
-                <TopProfileContactItem fa_class="fa-brands fa-github" type="link" text="pramotesupnoi" val="https://github.com/pramotesupnoi" />
-                <TopProfileContactItem fa_class="fa-brands fa-linkedin" type="link" text="pramotesupnoi" val="https://www.linkedin.com/in/pramote-supnoi-a94482152/" />
+                <TopProfileContactItem fa_class="fa-brands fa-github" type="link" text="/pramotesupnoi" val="https://github.com/pramotesupnoi" />
+                <TopProfileContactItem fa_class="fa-brands fa-linkedin" type="link" text="/in/pramote-supnoi" val="https://www.linkedin.com/in/pramote-supnoi-a94482152/" />
                 <TopProfileContactItem fa_class="fa-solid fa-square-phone" type="tel" text="+6695-169-9021" val="+66951699021" />
                 <TopProfileContactItem fa_class="fa-solid fa-envelope" type="mailto" text="pramotesupnoi@gmail.com" val="pramotesupnoi@gmail.com" />
               </div>
+              
             </div>
           </div>
+        </div>
+        <div className="bg-white p-3">
+          <a href={cv_file} className="btn btn-dark rounded-pill w-100" download>Download as PDF</a>
         </div>
     </div>
   );
